deaa36892f816669142916c4dcf4d74df42f34ce
jefaye <clement.jean.l.faye@gmail.com>
2024-10-12 16:12:57 +0000

    Todos

--- src/models.rs
+++ src/models.rs
@@ -1,1 +1,14 @@
-pub struct Vehicle {}
+pub struct Roads {
+    vehicules: Vec<Vehicle>
+}
+
+pub enum Color {
+    Red,
+    Blue,
+    Green,
+    Yellow,
+}
+
+pub struct Vehicle {
+    color: Color,
+}
